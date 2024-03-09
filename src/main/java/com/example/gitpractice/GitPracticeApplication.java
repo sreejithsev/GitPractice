c24d2ca49dadc8cbfe82a0cd7c7b01be5c48bce8
Sreejith S <sreejithsev@gmail.com>
2024-03-09 20:16:58 +0530

    Feature B car

--- src/main/java/com/example/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/example/gitpractice/GitPracticeApplication.java
@@ -9,6 +9,7 @@ public class GitPracticeApplication {
 	public static void main(String[] args) {
 		System.out.println("Gift from feature b");
 		System.out.println("Message from feature b");
+		System.out.println("Car from feature b");
 		SpringApplication.run(GitPracticeApplication.class, args);
         System.out.println("Feature/A(C1,C2)");
 	}
