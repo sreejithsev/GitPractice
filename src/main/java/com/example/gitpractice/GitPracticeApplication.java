b755a443478b9b9986cfc80e38d40e63a33dea28
Sreejith S <sreejithsev@gmail.com>
2024-03-09 20:52:13 +0530

    Feature B message

--- src/main/java/com/example/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/example/gitpractice/GitPracticeApplication.java
@@ -10,6 +10,7 @@ public class GitPracticeApplication {
 		System.out.println("Book from feature b");
 		System.out.println("Gift from feature a");
 		System.out.println("Gift from feature b");
+		System.out.println("Message from feature a");
 		System.out.println("Message from feature b");
 		System.out.println("Car from feature b");
 		System.out.println("Power from feature b");
