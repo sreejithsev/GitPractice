aa7b89dab12b39afa9bb965bbb26d04aad33b504
Sreejith S <sreejithsev@gmail.com>
2024-04-25 23:28:18 +0530

    Add message 2

--- src/main/java/com/example/gitpractice/SquashMergeChecker.java
+++ src/main/java/com/example/gitpractice/SquashMergeChecker.java
@@ -3,5 +3,6 @@ package com.example.gitpractice;
 public class SquashMergeChecker {
     public void checkConflictIfSquashed(){
         System.out.println("Feature k message 1");
+        System.out.println("Feature k message 2");
     }
 }
