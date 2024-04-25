63635c1892af038ad3b6e73e314bd29c3680653f
Sreejith S <42213030+sreejithsev@users.noreply.github.com>
2024-04-25 23:37:44 +0530

    Feature/k (#4)
    * Add message 1
    
    * Add message 2
    
    * Add message 3

--- src/main/java/com/example/gitpractice/SquashMergeChecker.java
+++ src/main/java/com/example/gitpractice/SquashMergeChecker.java
@@ -2,6 +2,8 @@ package com.example.gitpractice;
 
 public class SquashMergeChecker {
     public void checkConflictIfSquashed(){
-
+        System.out.println("Feature k message 1");
+        System.out.println("Feature k message 2");
+        System.out.println("Feature k message 3");
     }
 }
