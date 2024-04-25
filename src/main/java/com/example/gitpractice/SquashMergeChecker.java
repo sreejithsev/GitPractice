b074cdf770f2d36180e7f18f0fb14b3187c0e914
Sreejith S <sreejithsev@gmail.com>
2024-04-26 00:15:58 +0530

    Add message 2 from l

--- src/main/java/com/example/gitpractice/SquashMergeChecker.java
+++ src/main/java/com/example/gitpractice/SquashMergeChecker.java
@@ -5,5 +5,8 @@ public class SquashMergeChecker {
         System.out.println("Feature k message 1");
         System.out.println("Feature k message 2");
         System.out.println("Feature k message 3");
+
+        System.out.println("Feature l message 1");
+        System.out.println("Feature l message 2");
     }
 }
