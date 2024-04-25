7dfcdb28a4d3a9d17151699496e020be5990ab28
Sreejith S <sreejithsev@gmail.com>
2024-04-25 23:08:14 +0530

    add message 2

--- src/main/java/com/example/gitpractice/CommitIDChecker.java
+++ src/main/java/com/example/gitpractice/CommitIDChecker.java
@@ -3,5 +3,6 @@ package com.example.gitpractice;
 public class CommitIDChecker {
     public void checkingCommitID(){
         System.out.println("Feature x message 1");
+        System.out.println("Feature x message 2");
     }
 }
