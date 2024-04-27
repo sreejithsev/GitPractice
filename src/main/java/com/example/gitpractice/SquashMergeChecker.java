973d883d4ccfc178af4c661257bc0ed113489682
Sreejith S <sreejithsev@gmail.com>
2024-04-27 13:37:37 +0530

    add new LEC message 3

--- src/main/java/com/example/gitpractice/SquashMergeChecker.java
+++ src/main/java/com/example/gitpractice/SquashMergeChecker.java
@@ -12,5 +12,6 @@ public class SquashMergeChecker {
 
         System.out.println("Feature LEC message 1");
         System.out.println("Feature LEC message 2");
+        System.out.println("Feature LEC message 3");
     }
 }
