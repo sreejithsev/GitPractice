6341c680a3cfa24ee2efc3335fe31f1b2177a2a0
Sreejith S <sreejithsev@gmail.com>
2024-04-27 15:15:46 +0530

    add new os message 2

--- src/main/java/com/example/gitpractice/LineEndingConflictChecker.java
+++ src/main/java/com/example/gitpractice/LineEndingConflictChecker.java
@@ -3,5 +3,6 @@ package com.example.gitpractice;
 public class LineEndingConflictChecker {
     public void checkLineEndingConflict(){
         System.out.println("Feature os message 1");
+        System.out.println("Feature os message 2");
     }
 }
