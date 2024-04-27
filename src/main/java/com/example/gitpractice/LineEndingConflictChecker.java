3b1356f91445a9f63ba8b38c5838e7c6e5b3b9ea
sreejith@linix <sreejithsev@gmail.com>
2024-04-27 15:39:03 +0530

    Merge branch 'feature/os' into master

---
+++ src/main/java/com/example/gitpractice/LineEndingConflictChecker.java
@@ -0,0 +1,8 @@
+package com.example.gitpractice;
+
+public class LineEndingConflictChecker {
+    public void checkLineEndingConflict(){
+        System.out.println("Feature os message 1");
+        System.out.println("Feature os message 2");
+    }
+}
