a179fe726fd24f3a51ac21f2bc196bf458fa144f
Sreejith S <sreejithsev@gmail.com>
2024-05-04 18:55:19 +0530

    add feature n message 1

--- src/main/java/com/example/gitpractice/LineEndingConflictChecker.java
+++ src/main/java/com/example/gitpractice/LineEndingConflictChecker.java
@@ -4,5 +4,6 @@ public class LineEndingConflictChecker {
     public void checkLineEndingConflict(){
         System.out.println("Feature os message 1");
         System.out.println("Feature os message 2");
+        System.out.println("Feature n message  1");
     }
 }
